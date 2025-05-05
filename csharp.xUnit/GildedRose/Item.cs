ee8a024a955702bcddcc6a590608d74101ddfc59
Clare Sudbery <clareworking@claresudbery.co.uk>
2025-05-05 18:45:32 +0100

    Merge with latest main on Emily's base

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -1,14 +1,13 @@
-﻿namespace GildedRoseKata
+namespace GildedRoseKata;
+
+public class Item
 {
-    public class Item
+    public string Name { get; set; }
+    public int SellIn { get; set; }
+    public int Quality { get; set; }
+
+    public override string ToString()
     {
-        public string Name { get; set; }
-        public int SellIn { get; set; }
-        public int Quality { get; set; }  
-        
-        // public override string ToString()
-        // {
-        //     return this.Name + ", " + this.SellIn + ", " + this.Quality;
-        // }  
+        return this.Name + ", " + this.SellIn + ", " + this.Quality;
     }
 }
